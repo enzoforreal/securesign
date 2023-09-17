9a42c1633b20bac3e26789379ed5a3b6942180ca
enzoforreal <enzoforreal@gmail.com>
2023-09-17 15:29:14 +0000

    files added

---
+++ pkg/sign/sign.go
@@ -0,0 +1,126 @@
+package sign
+
+import (
+    "errors"
+    "time"
+    // Autres imports nécessaires
+)
+
+// Document représente un document à signer
+type Document struct {
+    ID        int       // Identifiant du document
+    Content   string    // Contenu du document
+    Signatures []Signature // Liste des signatures électroniques associées au document
+    CreatedAt time.Time // Horodatage de création du document
+}
+
+// Signature représente une signature électronique
+type Signature struct {
+    Signer    string    // Nom du signataire
+    Timestamp time.Time // Horodatage de la signature électronique
+}
+
+// Signez électroniquement un document avec la signature du signataire
+func SignDocument(doc *Document, signer string) error {
+    // Vérifiez si le document n'a pas déjà été signé par le même signataire
+    for _, sig := range doc.Signatures {
+        if sig.Signer == signer {
+            return errors.New("Ce signataire a déjà signé ce document.")
+        }
+    }
+
+    // Appliquez la signature électronique en ajoutant le signataire et l'horodatage
+    signature := Signature{
+        Signer:    signer,
+        Timestamp: time.Now(),
+    }
+
+    // Ajoutez la signature à la liste des signatures du document
+    doc.Signatures = append(doc.Signatures, signature)
+
+    // Enregistrez les modifications dans la base de données ou un système de stockage approprié
+    // Assurez-vous que le document est mis à jour avec les signatures
+
+    return nil
+}
+
+// Vérifie la validité d'une signature électronique
+func VerifySignature(signature string) bool {
+    // Mettez en œuvre la logique de vérification de la signature ici
+    // Renvoyez true si la signature est valide, sinon false
+
+    // Exemple de logique de vérification simplifiée
+    return signatureIsValid(signature)
+}
+
+// Applique la signature électronique à un document
+func ApplySignature(document *Document, signer string) error {
+    // Mettez en œuvre la logique d'application de la signature ici
+    // Assurez-vous que le document est marqué comme signé après l'application de la signature
+
+    // Exemple de logique d'application de signature
+    if document.IsSignedBy(signer) {
+        return errors.New("Ce signataire a déjà signé ce document.")
+    }
+
+    document.AddSignature(signer)
+    return nil
+}
+
+// Récupère un document par son ID
+func GetDocumentByID(id int) (*Document, error) {
+    // Mettez en œuvre la logique de récupération du document par son ID ici
+    // Renvoyez le document trouvé ou une erreur si le document n'est pas trouvé
+
+    // Exemple de logique de récupération de document
+    document, err := retrieveDocumentFromDatabase(id)
+    if err != nil {
+        return nil, err
+    }
+    return document, nil
+}
+
+// Exemple de logique de vérification de signature simplifiée
+func signatureIsValid(signature string) bool {
+    // Implémentez votre logique de vérification ici
+    // Renvoyez true si la signature est valide, sinon false
+    // Par exemple, vérifiez une signature avec une clé publique ou un algorithme de hachage
+    return true
+}
+
+// Exemple de méthode pour ajouter une signature au document
+func (d *Document) AddSignature(signer string) {
+    signature := Signature{
+        Signer:    signer,
+        Timestamp: time.Now(),
+    }
+    d.Signatures = append(d.Signatures, signature)
+}
+
+// Exemple de méthode pour vérifier si un signataire a déjà signé le document
+func (d *Document) IsSignedBy(signer string) bool {
+    for _, sig := range d.Signatures {
+        if sig.Signer == signer {
+            return true
+        }
+    }
+    return false
+}
+
+// Exemple de méthode pour récupérer un document à partir de la base de données
+func retrieveDocumentFromDatabase(id int) (*Document, error) {
+    // Mettez en œuvre la logique de récupération du document depuis la base de données
+    // Renvoyez le document trouvé ou une erreur si le document n'est pas trouvé
+
+    // Exemple de récupération de document fictive (à remplacer par la logique réelle)
+    if id == 1 {
+        return &Document{
+            ID:        1,
+            Content:   "Contenu du document de test",
+            Signatures: []Signature{},
+            CreatedAt: time.Now(),
+        }, nil
+    }
+
+    return nil, errors.New("Document non trouvé")
+}
