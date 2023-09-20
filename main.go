9da842fd2b8ab6b90e05298e977768e3f5dc400d
enzoforreal <enzoforreal@gmail.com>
2023-09-20 11:21:57 +0000

    new updated

--- main.go
+++ main.go
@@ -8,23 +8,23 @@ import (
 )
 
 func main() {
-	// Initialise GIN
+	
 	r := gin.Default()
 
-	// Définit un répertoire pour les fichiers statiques (CSS, JS)
+	
 	r.Static("/static", "./web/static")
 
-	// Charge les modèles HTML du répertoire web/templates
+	
 	r.LoadHTMLGlob("web/templates/*")
 
-	// Route pour la page d'accueil
+	
 	r.GET("/", handlers.HomeHandler)
 
-	// Routes pour l'envoi d'e-mails et la signature électronique
+	
 	r.POST("/send-email", handlers.SendEmailHandler)
 	r.POST("/sign-document", handlers.SignDocumentHandler)
 
-	// Lance le serveur sur le port 8080
+	
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
