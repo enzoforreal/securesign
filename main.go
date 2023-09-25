dcf1f220be74cabab469e5a188caa546ccfce55e
enzoforreal <enzoforreal@gmail.com>
2023-09-25 16:17:26 +0200

    main updated

--- main.go
+++ main.go
@@ -2,29 +2,22 @@ package main
 
 import (
 	"GenSecureSign/handlers"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	
+
 	r := gin.Default()
 
-	
 	r.Static("/static", "./web/static")
 
-	
 	r.LoadHTMLGlob("web/templates/*")
 
-	
 	r.GET("/", handlers.HomeHandler)
 
-	
 	r.POST("/send-email", handlers.SendEmailHandler)
 	r.POST("/sign-document", handlers.SignDocumentHandler)
 
-	
-	http.Handle("/", r)
-	http.ListenAndServe(":8080", nil)
+	r.Run(":8080")
 }
